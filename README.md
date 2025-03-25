2557c22cd249cfec9c55e2cffd5419dd74eb92e3
Rajesh kumar <rajesh8368568776@gmail.com>
2025-03-25 10:38:32 +0530

    this is templates and dockerfile or app updation

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 ## End To End Machine Learning Project
+Run from terminal:
+docker build -t testdockerkrish.azurecr.io/mltest:latest .
+
+docker login testdockerkrish.azurecr.io
+
+docker push testdockerkrish.azurecr.io/mltest:latest
--- src/logger.py
+++ src/logger.py
@@ -2,37 +2,15 @@ import logging
 import os
 from datetime import datetime
 
-# Define log file name
-LOG_FILE = f"{datetime.now().strftime('%y-%m-%d')}.log"
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
 
-# Define log directory
-logs_path = os.path.join(os.getcwd(), "logs")
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
-# Create directory if it doesn't exist
-os.makedirs(logs_path, exist_ok=True)
-
-# Define full log file path
-LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
-
-# Configure logging
 logging.basicConfig(
     filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
-    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s"
+    datefmt ="%m/%d/%y %I:%M:%S %p"
 )
-
-class Logger:
-    @staticmethod
-    def info(message):
-        logging.info(message)
-
-    @staticmethod
-    def error(message):
-        logging.error(message)
-
-    @staticmethod
-    def warning(message):
-        logging.warning(message)
-
-if __name__=="__main__":
-  logging.info("logging has been configured successfully")
